0bab608fb2a082a53268048536cc129e2d98a6ec
Amy Chen <amychen@mac.dhcp.wustl.edu>
2026-07-27 18:17:47 -0500

    changing mpi

--- hw4/mpi.sh
+++ hw4/mpi.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
-#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=2
+#SBATCH --ntasks=2
 #SBATCH -c 1
 #SBATCH -p ib-linuxlab
-#SBATCH -J hw-4-test
+#SBATCH -J hw4-test
 #SBATCH -A engr-class-any
 #SBATCH --output=./run-outputs/mpi-Nbody.%j
 
 echo "Running on host: $(hostname)"
-module add tau-mpi
-cd /<PATH>/<TO>/hw4
-#parameters are: numstars starmass t_final display (display may be 1 for local run)
-mpirun /<PATH>/<TO>/hw4/Nbody 1024 200.0 1000 0
+module add mpi/mpich-x86_64
+cd /home/cyue/cse5606/hw4
+mpirun ./Nbody 512 200.0 1000 0
